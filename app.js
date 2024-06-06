e47ca706340f9ead8041e76e87b12535d4cd3fd2
Heisnam Sunrose <johnsonheisnam02@gmail.com>
2024-06-06 22:30:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+  1.This is the root file of node js application. 
+  2.Named anything but generally named as app.js or server.js.
+
+  Core modules of node.js
+  ============================
+  http => Launch a server, send requests
+  https => Launch a SSL server
+  fs
+  path
+  os  
+*/
+
+
+const http = require("http");
